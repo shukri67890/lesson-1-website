27cff0576bea45ca7924823b57076e6956294374
shukri67890 <90867214+shukri67890@users.noreply.github.com>
2021-09-16 13:17:28 -0600

    Add files via upload

---
+++ Main_ Program_File_for_Github.py
@@ -0,0 +1,16 @@
+#Main program File for Github Module
+print()
+print("Welcome to the github module program")
+
+import Main_Program_file_for_Github
+
+num_1 = Main_Program_file_for_Github.a
+num_2 = Main_Program_file_for_Github.b
+num_3 = Main_Program_file_for_Github.c
+num_4 = Main_Program_file_for_Github.d      
+num_5 = Main_Program_file_for_Github.e   
+
+print("We will now access the data from the module!")
+print (num_1, "+" , num_2, "+", "=", num_1 + num_2)
+print ( num_3, "*", num_4), num_5, "=", num_3 * num_4 * num_5
+print ("magic")
